f39ad6aaee1714c14e5976bd633b93c44cdc774b
jarenella <jamesa122333@gmail.com>
2022-11-22 17:32:41 -0800

    added basic structural elements and css styling

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], //where u want to use the css file gnerated by tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
