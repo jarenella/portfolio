95ff69da3d4bdc22b27740e15c2fa94e42c216cb
jarenella <jamesa122333@gmail.com>
2022-11-24 14:15:14 -0800

    developed further modal scripts

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
+//function for closing modal by hiding it
+function closeModal() {
+    modalBackground.classList.add("hidden");
+    modal.classList.add("hidden");
+}
+
+//when the modal exit button is clicked, the modal is exited by being hidden
 const modal = document.querySelector(".modal")
 const modalBackground = document.querySelector(".modal-background");
 const modalButton = document.querySelector(".modal-button");
-
-//when the modal exit button is clicked, the modal is exited by being hidden
 modalButton.addEventListener("click", () => {
-    modalBackground.classList.add("hidden");
-    modal.classList.add("hidden");
+    closeModal();
 });
+//when the modal background is clicked (someone clicks outside of the modal), the modal is also closed
+modalBackground.addEventListener("click", () => {
+    closeModal();
+})
 
 //when the main card is clicked, the user is sent to the link
 const heroCard = document.querySelector(".hero-card");
